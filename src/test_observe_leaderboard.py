0ec7d67847f387b5da0e8251159ce3747ca528c0
John Lee <jlee88@nd.edu>
2023-04-26 19:01:16 -0400

    leaderboard obs

---
+++ src/test_observe_leaderboard.py
@@ -0,0 +1,40 @@
+from StockMarketEndpoint import *
+import sys
+import signal
+import time
+import re
+from collections import defaultdict
+
+if __name__ == '__main__':
+    
+    # init stock market endpoint
+    sm = StockMarketEndpoint(name=sys.argv[1], username=sys.argv[2], password=sys.argv[2])
+    sm.register(registered_ok=True)
+    
+    file = "leaderboard.txt"
+    
+    net_worths = defaultdict(list)
+    
+    while True:
+        time.sleep(60)
+        lb = sm.get_leaderboard()
+        
+        # parse leaderboard
+        for row in lb:
+            m = re.search("(.*) | (.*)\n", row)
+            if m is None:
+                continue
+            else:
+                net_worths[m.group(1)].append(float(m.group(2)))
+                
+        # write output
+        with open(file, 'a') as f:
+            for user in sorted(net_worths.keys()):
+                f.write(net_worths[user][-1], end = ' ')
+            f.write('\n')
+            
+            
+        
+            
+                
+        
