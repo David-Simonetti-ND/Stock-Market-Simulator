97d9a1dfca630f438136695a21eaea2885e0a6f3
David Simonetti <dsimone2@nd.edu>
2023-04-27 14:42:01 -0400

    broker strategy

--- src/StockMarketBroker.py
+++ src/StockMarketBroker.py
@@ -261,7 +261,7 @@ def main():
         while len(readable) > 0:
             if start_time == -1:
                 start_time = time.time()
-            if (total_requests_handled % 1_000) <= 100:
+            if (total_requests_handled % 1_000) <= 10:
                 print("Average throughput: ", total_requests_handled / (time.time() - start_time))
             # randomly pick a client to service
             conn = random.choice(readable)
@@ -312,18 +312,19 @@ def main():
                     conn.sendall(format_message(server.start_request(request, conn)))
                 except Exception:
                     pass
-        to_try = []
-        for key in server.pending_reqs.keys():
-            if server.chain_sockets[key] in server.name_to_conn.keys() or len(server.pending_reqs[key]) == 0:
-                continue
-            to_try.append((index, server.pending_reqs[key][0]))
-        for index, (request, attempted_conn) in to_try:
-            chain_sock = server.chain_sockets[server.hash(request["username"]) % server.num_chains]
-            chain_servicer = server.start_request(request, attempted_conn)
-            if chain_servicer != None:
-                server.name_to_conn[chain_servicer] = attempted_conn
-                server.pending_conns.add(attempted_conn)
-                server.pending_reqs[index].remove((request, attempted_conn))
+        if (total_requests_handled % 1_000) <= 10:
+            to_try = []
+            for key in server.pending_reqs.keys():
+                if server.chain_sockets[key] in server.name_to_conn.keys() or len(server.pending_reqs[key]) == 0:
+                    continue
+                to_try.append((index, server.pending_reqs[key][0]))
+            for index, (request, attempted_conn) in to_try:
+                chain_sock = server.chain_sockets[server.hash(request["username"]) % server.num_chains]
+                chain_servicer = server.start_request(request, attempted_conn)
+                if chain_servicer != None:
+                    server.name_to_conn[chain_servicer] = attempted_conn
+                    server.pending_conns.add(attempted_conn)
+                    server.pending_reqs[index].remove((request, attempted_conn))
 
 
 
