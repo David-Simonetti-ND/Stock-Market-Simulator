09aadc55ce1a250227a61fcb36111d977f734606
David Simonetti <dsimone2@nd.edu>
2023-04-27 14:41:23 -0400

    broker strategy

--- src/StockMarketBroker.py
+++ src/StockMarketBroker.py
@@ -316,8 +316,8 @@ def main():
         for key in server.pending_reqs.keys():
             if server.chain_sockets[key] in server.name_to_conn.keys() or len(server.pending_reqs[key]) == 0:
                 continue
-            to_try.append(server.pending_reqs[key][0])
-        for request, attempted_conn in to_try:
+            to_try.append((index, server.pending_reqs[key][0]))
+        for index, (request, attempted_conn) in to_try:
             chain_sock = server.chain_sockets[server.hash(request["username"]) % server.num_chains]
             chain_servicer = server.start_request(request, attempted_conn)
             if chain_servicer != None:
